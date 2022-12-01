5e2474ca431917dd358ec155fde03058747a65dc
Witalo Rebouças <tilowr@gmail.com>
2022-11-30 23:59:06 -0300

    Update form.php
    removi caracter sobrando no registro da msg de sessão

--- form.php
+++ form.php
@@ -29,9 +29,7 @@
 	  <?php
 	  if(isset($_SESSION['msg']))
 		  echo $_SESSION['msg'];
-			unset ($_SESSION['msg'])/;
-	  
-	  
+			unset ($_SESSION['msg']);
 	  ?>
     </header>
     <form class="box-login" method="post" action="processa.php">
